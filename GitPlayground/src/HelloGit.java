9d37a1a9ce0c21ffc133513bd11373b8fbd9646c
Training <Training@192.168.252.215>
2020-03-07 14:49:32 +0530

    //Changed gitPlayground

--- GitPlayground/src/HelloGit.java
+++ GitPlayground/src/HelloGit.java
@@ -3,6 +3,10 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Gitt!!!!!!!!");
+		System.out.println("Hello Gitt Changed!!!!!!!!");
+		System.out.println("Hello Gitt changed!!!!!!!!");
+		System.out.println("Hello Gitt changed!!!!!!!!");
+		System.out.println("Hello Gitt Changed!!!!!!!!");
 
 	}
 
