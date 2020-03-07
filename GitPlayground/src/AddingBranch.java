2e900f885ab3562054474b2b34a543105055fe53
Training <Training@192.168.252.215>
2020-03-07 15:47:13 +0530

    //changed adding branch

---
+++ GitPlayground/src/AddingBranch.java
@@ -0,0 +1,12 @@
+
+public class AddingBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("adding branch");
+		System.out.println("adding branch");
+		System.out.println("adding branch");
+		System.out.println("adding branch");
+	}
+
+}
